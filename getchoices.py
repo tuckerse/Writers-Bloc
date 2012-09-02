88a7a7fcff465326798c7edb07195968c44e8e7c
Chris Evans <cevans216@gmail.com>
2012-09-02 01:36:26 -0400

    Move getchoices to its own file

---
+++ getchoices.py
@@ -0,0 +1,19 @@
+import logging
+
+from basehandler import BaseHandler
+from Models import Game
+from django.utils import simplejson as json
+
+class GetChoices(BaseHandler):
+    def post(self):
+        if not self.user:
+            logging.critical('Invalid choice fetching detected!')
+        else:
+            info = json.loads(self.request.body)
+            game_id = str(info['game_id'])
+            game = Game.get_by_key_name(game_id)
+            #game = retrieveCache(game_id, Game)
+            json_string = json.dumps({"choices": game.next_parts})
+            self.response.out.write(json_string);
+
+        return
--- storybook.py
+++ storybook.py
@@ -169,20 +169,6 @@ def getPlayerNames(game):
         #nameList.append(trimName(retrieveCache(user, User).name))
     return nameList
 
-class GetChoices(BaseHandler):
-    def post(self):
-        if not self.user:
-            logging.critical('Invalid choice fetching detected!')
-        else:
-            info = json.loads(self.request.body)
-            game_id = str(info['game_id'])
-            game = Game.get_by_key_name(game_id)
-            #game = retrieveCache(game_id, Game)
-            json_string = json.dumps({"choices": game.next_parts})
-            self.response.out.write(json_string);
-
-        return
-
 class JoinGame(BaseHandler):
     def post(self):
         if not self.user:
