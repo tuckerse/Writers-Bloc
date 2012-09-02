2f1c1136d24b7e41b159cc16a60058c40cc43717
Chris Evans <cevans216@gmail.com>
2012-09-02 00:06:49 -0400

    Move FindGame screen to separate file

---
+++ findgame.py
@@ -0,0 +1,13 @@
+class FindGame(BaseHandler):
+    def get(self):
+        if not self.user:
+            self.render(u'login_screen')
+        else:
+            game_id = findGame(self.user)
+
+            if game_id is None:
+                self.redirect("/")
+                return
+
+            self.redirect("/waiting_to_start?" + urllib.urlencode({'game_id':game_id}))
+
