84c3dfc0eb3fffaf2b8d9aa0bd056d23a9aa8469
Tim Vergenz <vergenzt@gmail.com>
2012-09-02 13:48:44 -0400

    Break long line into multiple lines

--- storybook.py
+++ storybook.py
@@ -146,7 +146,11 @@ class GetLobby(BaseHandler):
             games = getLobbyGames()
             response = {'games':[]}
             for game in games:
-                response['games'].append({'game_id':game.game_id, 'current_players':game.current_players, 'end_sentence':game.end_sentence})
+                response['games'].append({
+                    'game_id':game.game_id,
+                    'current_players':game.current_players,
+                    'end_sentence':game.end_sentence
+                })
             self.response.out.write(json.dumps(response))
         else:
             logging.critical("Unauthorized lobby post request")
