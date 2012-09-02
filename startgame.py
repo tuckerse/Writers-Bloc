0c2fc7f9c790f5864a07a619e80f89f81ff68958
Chris Evans <cevans216@gmail.com>
2012-09-02 01:24:41 -0400

    Move startgame to its own file

---
+++ startgame.py
@@ -0,0 +1,28 @@
+from basehandler import BaseHandler
+from UserHandler import User
+from cacheLib import canPlayerHost, markPlayerHostedGame, retrieveCache
+from DefaultStartSentences import defaultStart
+from DefaultEndSentences import defaultEnd
+from helperfunctions import joinGame
+
+class StartGame(BaseHandler):
+    def post(self):
+        if not self.user:
+            self.render(u'login_screen')
+        else:
+            if canPlayerHost(self.user.user_id):
+                end_sentence = self.request.get('end_sentence')
+                start_sentence = self.request.get('start_sentence')
+                if end_sentence == "":
+                    end_sentence = defaultEnd.getRandomDefault()
+                    if start_sentence == "":
+                        start_sentence = defaultStart.getRandomDefault()
+                game_id = initializeGame(getNextGameID(), MAX_PLAYERS, start_sentence, end_sentence)
+                markPlayerHostedGame(self.user.user_id)
+                #joinGame(self.user, game_id)
+                joinGame(retrieveCache(self.user.user_id, User), game_id)
+                self.render(u'game_created_screen', game_id=str(game_id), MAX_PLAYERS=MAX_PLAYERS)
+            else:
+                self.render(u'cant_host_yet')
+        return
+
