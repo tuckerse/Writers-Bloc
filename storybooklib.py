c7c74c4ef6fac50568420419d72d9c45386a548d
Chris Evans <cevans216@gmail.com>
2012-09-30 15:17:56 -0400

    Remove uneeded function call and resolve error in achievement framework

--- storybooklib.py
+++ storybooklib.py
@@ -345,16 +345,10 @@ def removeAFKVotes(game):
 def finishGame(game):
     game.finished = True
     game.game_ended = datetime.datetime.now()
-    resolveAchievements(game)
+    applyAchievements(game)
     #game.put()
     #^^ Will need to uncomment this if you change the put() in the achievement framework
     #storeCache(game, str(game.game_id))
-
-def resolveAchievements(game):
-    #Will implement this tomorrow
-    #achievements is a list of dictionaries keys=[(winner_id, achievement_id, score]
-    achievements = applyAchievements(game)
-    storeAchievementData(achievements, game)
 
 def removeUser(game_id, user_id):
     game = Game.get_by_key_name(game_id)
