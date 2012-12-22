479601f5e8ef75e9825b5574a71a38eeca3efa20
Chris Evans <cevans216@gmail.com>
2012-12-21 19:07:26 -0500

    Rejoin disconnected game only shows when the game has actually been started. Closes issue #116

--- menupage.py
+++ menupage.py
@@ -1,5 +1,6 @@
 from basehandler import BaseHandler
 from cacheLib import storeCache
+from Models import Game
 
 class MenuPage(BaseHandler):
     def get(self):
@@ -11,4 +12,8 @@ class MenuPage(BaseHandler):
                 storeCache(self.user, self.user.user_id)
                 self.render(u'user_settings')
             else:
-                self.render(u'menu_screen', in_game=(not self.user.current_game is None), game_id=self.user.current_game)
+                in_game = False
+                if not self.user.current_game is None:
+                    game = Game.get_by_key_name(str(self.user.current_game))
+                    in_game = game.started 
+                self.render(u'menu_screen', in_game=in_game, game_id=self.user.current_game)
