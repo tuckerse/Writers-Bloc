727f745d2bccc0422d326ec2414615316aa1d485
Chris Evans <cevans216@gmail.com>
2013-01-02 23:28:21 -0500

    Add finishGame import in displaycompleteverification. #127

--- displaycompleteverification.py
+++ displaycompleteverification.py
@@ -3,7 +3,7 @@ import datetime
 
 from basehandler import BaseHandler
 from Models import Game
-from storybooklib import getStoryStringForGameScreen, getScoreInfo, getProfilesAndAFKS, changeToSubmissionPhase
+from storybooklib import getStoryStringForGameScreen, getScoreInfo, getProfilesAndAFKS, changeToSubmissionPhase, finishGame
 from django.utils import simplejson as json
 
 class DisplayCompleteVerification(BaseHandler):
