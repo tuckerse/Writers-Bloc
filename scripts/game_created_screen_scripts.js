16ad402a5eeda990db4fc25a64c95711b7bef059
Chris Evans <cevans216@gmail.com>
2013-01-27 18:42:15 -0500

    Add button iamges to the game created screen.

--- scripts/game_created_screen_scripts.js
+++ scripts/game_created_screen_scripts.js
@@ -16,9 +16,9 @@ function statusCheck()
 			var num_players = response.num_players;
 			document.getElementById("current_players").innerHTML = num_players + "/" + MAX_PLAYERS + " Players<br>Current Players:<br>";
             if(num_players > 1)
-                document.getElementById("early_start").disabled = false;
+                document.getElementById("early_start").type = "image";
             else if(num_players == 1)
-                document.getElementById("early_start").disabled = true;
+                document.getElementById("early_start").type = "hidden";
 
 			var players = response.players;
 			for(i in players)
